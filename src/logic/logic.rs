b62da5c54cf6ebe96d1111d7f854795b421bb87c
Sironi-00 <0sironi@gmail.com>
2022-10-07 19:30:41 +0100

    Todo with loop before get input

--- src/logic/logic.rs
+++ src/logic/logic.rs
@@ -16,8 +16,8 @@ impl Todo {
     pub fn view(&self) {
         println!("ID: {id} Title: {title} Complete: {complete}\n * {note} ",id=self.id , title=self.title, complete=self.complete, note=self.note)
     }
-    pub fn obj(&self) -> (u8, String) {
-        (self.id, self.title)
+    pub fn obj(&self) -> (u8, &String) {
+        (self.id, &self.title)
     }
     pub fn complete(&mut self) {
         self.complete= !{self.complete}
--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -1,34 +1,44 @@
 mod logic;
-pub fn run() { 
-let todo_list: Vec<logic::Todo> = Vec::with_capacity(8);
-//loop 
+pub fn run() {
+    let mut todo_list: Vec<logic::Todo> = Vec::with_capacity(8);
+    loop {
+        println!("Hello Wolcome to my cli TodoApp in rust\n select function add view complete remove quit");
+        let selected = "add";
+        match selected {
+            "add" => {
+                println!("enter ID Title Note");
+                todo_list.push(logic::Todo::add(0, "Test", ""));
+            }
+            "view" => {
+                if todo_list.len() == 0 {
+                    println!("Add elements first");
+                    //continue
+                }
+                // Ability to enter Id to select todo
+                for td in &todo_list {
+                    td.view();
+                }
+            }
+            "complete" => {
+                let get_id = 0;
+                for td in &mut todo_list {
+                    if get_id == td.obj().0 {
+                        td.complete()
+                    }
+                }
+            }
+            "remove" => {
+                let get_id = 0;
+                for td in &todo_list {
+                    if get_id == td.obj().0 {
+                        td.view()
+                    }
+                }
+            }
+            "quit" | "q" | "exit" => println!("end"), //break;
+            _ => println!("Err"),
+        }
 
-    println!("Hello Wolcome to my cli TodoApp in rust\n select function add view complete remove quit");
-    let mut selected = "add";
-    match selected {
-        "add" => {
-            if todo_list.len() == todo_list.len() {
-                println!("Array Full");
-                //continue
-            }
-            println!("enter ID Title Note");
-            todo_list[0] = logic::Todo::add(0, "Test", "");
-        }, "view" => {
-            if todo_list.len() == 0 {
-                println!("Add elements first");
-                //continue
-            }
-            // Ability to enter Id to select todo
-            for td in todo_list {
-                td.view();
-            }
-        }, "remove" => {
-            let get_id = 0;
-            for td in todo_list {
-                if get_id == td.obj().0 {td.view()}
-            }
-        }, "quit" | "q" | "exit" => println!("end")//break;
+        todo_list[0].view()
     }
-
-
 }
