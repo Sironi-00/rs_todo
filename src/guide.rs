215d1087f1ce861a67a4f412caa12e47c540542e
Sironi-00 <0sironi@gmail.com>
2022-10-09 15:47:28 +0100

    Refractored the code for heirachy

---
+++ src/guide.rs
@@ -0,0 +1,7 @@
+pub fn _to_imp() {
+    println!("    // loop >  conditionals (add, view, complete, remove, Quit)
+    // adding + Store > _Index, Title, Note, _Complete
+    // View > All or Each
+    // Complete > toggler
+    // Remove > Selected \n\n\n");
+}
