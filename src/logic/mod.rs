a28a496c4fb9f4107aae2431b7b101e7629a6b07
Sironi-00 <0sironi@gmail.com>
2022-10-11 01:04:39 +0100

    Successfully added Storing but unable to save empty todos

--- src/logic/mod.rs
+++ src/logic/mod.rs
@@ -1,29 +1,40 @@
 // use std::io::prelude::*;
-use std::{fs::{OpenOptions, File}, io::{Write, Read}};
+use std::{
+    fs::{File, OpenOptions},
+    io::{Read, Write},
+};
 
 pub mod form;
 
 fn loc_file() -> File {
-    match OpenOptions::new().create(true).read(true).write(true).open("local.txt") {
+    match OpenOptions::new()
+        .create(true)
+        .read(true)
+        .write(true)
+        .open("local.txt")
+    {
         Ok(n) => n,
-        Err(e)=> panic!("Error: {}", e)
+        Err(e) => panic!("Error: {}", e),
     }
 }
 
 pub fn local_read() -> Vec<form::Todo> {
     // Gets Todos from a file
     let mut file = &loc_file();
-    
+
     let mut file_items: String = String::new();
-    
+
     match file.read_to_string(&mut file_items) {
         Ok(_) => file_items = file_items,
         Err(e) => {
             println!("Error: {} - While Reading Local Stored", e);
-    }};
+        }
+    };
 
     // if file has no contents - creates empty vec
-    if file_items.len() == 0 {return Vec::with_capacity(8)}
+    if file_items.len() == 0 {
+        return Vec::with_capacity(8);
+    }
 
     let mut list: Vec<form::Todo> = vec![];
 
@@ -33,27 +44,32 @@ pub fn local_read() -> Vec<form::Todo> {
         for item in line.split("|") {
             // adds the split line to a temp
             temp.push(item.trim());
-        };
+        }
         let id_from = temp[0].parse::<u8>().unwrap();
         list.push(form::Todo::add(id_from, temp[1]));
     }
     if list.len() > 0 {
         return list;
     } else {
-        return Vec::with_capacity(8)
+        return Vec::with_capacity(8);
     }
 }
 pub fn local_write(loc_td: &Vec<form::Todo>) {
     // Writes Todos to a file
     let file = &mut loc_file();
 
-    // Formats Todos n Write them
+    // An Empty String that stores contents to write
     let mut temp: String = String::new();
-    for i in loc_td {
-        temp = format!("{}|{}",i.obj().0, i.obj().1);
+    if loc_td.len() != 0 {
+        // Formats Todos n Write them
+        for i in loc_td {
+            temp += &format!("{}|{}\n", i.obj().0, i.obj().1)
+        }
+    } else {
+        temp = String::from(" ")
     }
-    match file.write(temp.as_bytes()) {
+    match file.write(temp.trim().as_bytes()) {
         Ok(_) => println!("Saving Complete"),
-        Err(e) => panic!("Error: {} - While Saving Data to local file", e) 
+        Err(e) => panic!("Error: {} - While Saving Data to local file", e),
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -121,9 +121,6 @@ pub fn run() {
                 println!("-----\nREMOVED\n------");
             }
             "quit" | "q" | "exit" => {
-                for td in &todo_list {
-                    td.view();
-                }
                 logic::local_write(&todo_list);
                 println!("Exiting...\nBye Bye!");
                 break;
